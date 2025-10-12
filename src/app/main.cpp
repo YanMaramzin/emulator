959b91407f86c910ab84c8c08b83c53cc6a4d895
mist <yan.maramzin@mail.ru>
2025-10-12 04:37:20 +0300

    [+] minimal test consoleLogger

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+import core.logger.console;
+import core.logger;
 
 int main()
 {
+    ConsoleLogger logger;
+    logger.log(LogLevel::Warning, "Hello World!");
     std::cout << "Hello World!\n";
 }
