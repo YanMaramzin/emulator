1badec690b71d33831d6d0acef4822412207d209
mist <yan.maramzin@mail.ru>
2025-10-21 02:43:52 +0300

    [+] MainWindow plugin added to launch the UI in the main function

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,8 +1,18 @@
 import core.logger.console;
 import core.logger;
 import core.pluginmanager;
+import mainwindow;
 
 int main()
 {
+    // Получаем UI плагин
+    auto windowPlugin = pluginManager.get<MainWindow>("MainWindow");
+    if (!windowPlugin) {
+        return 0;
+    }
+
+    windowPlugin->show();
+
+    pluginManager.shutdownAll();
     return 0;
 }
