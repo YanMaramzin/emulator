78645e732aa0c983248bada853332b4ecda43c36
mist <yan.maramzin@mail.ru>
2025-10-23 05:05:54 +0300

    [*] temp commit

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,19 +1,19 @@
-#include <compare>
-import core.logger.console;
-import core.logger;
-import core.pluginmanager;
-import mainwindow;
+// #include <compare>
+// import core.logger.console;
+// import core.logger;
+// import core.pluginmanager;
+// import mainwindow;
 
 int main()
 {
-    // Получаем UI плагин
-    auto windowPlugin = pluginManager.get<MainWindow>("MainWindow");
-    if (!windowPlugin) {
-        return 0;
-    }
-
-    windowPlugin->show();
-
-    pluginManager.shutdownAll();
+    // // Получаем UI плагин
+    // auto windowPlugin = pluginManager.get<MainWindow>("MainWindow");
+    // if (!windowPlugin) {
+    //     return 0;
+    // }
+    //
+    // windowPlugin->show();
+    //
+    // pluginManager.shutdownAll();
     return 0;
 }
